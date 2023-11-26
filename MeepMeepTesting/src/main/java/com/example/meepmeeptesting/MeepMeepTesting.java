52bdaa05e462dc29123af20497272118cb04c308
User <tevel.ftc@gmail.com>
2023-11-26 18:35:59 +0200

    First path

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -13,16 +14,15 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-35, 35, -Math.PI/2))
+                                .splineTo(new Vector2d(-30.43, 15.38), Math.toRadians(-40.91))
+                                .splineTo(new Vector2d(-15.38, 11.35), Math.toRadians(0.00))
+                                .splineTo(new Vector2d(13.04, 14.95), Math.toRadians(41.38))
+                                .splineTo(new Vector2d(33.19, 60.12), Math.toRadians(3.07))
+                                .splineTo(new Vector2d(61.61, 61.40), Math.toRadians(0.00))
+
                                 .build()
+
                 );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Util.java
@@ -0,0 +1,11 @@
+package com.example.meepmeeptesting;
+
+public class Util {
+    public static double toFeet(double meters) {
+        return meters * 3.28084;
+    }
+
+    public static double toMeters(double feet) {
+        return feet / 3.28084;
+    }
+}
